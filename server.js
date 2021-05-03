693b9c26dc9dbf0aa86f9710d105c5ffb42d7c43
jgallaway2011 <jgallaway2011@gmail.com>
2021-05-03 16:28:57 -0400

    Updated mongoose settigns

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
 // Start the API server
 app.listen(PORT, function() {
